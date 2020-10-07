30c82df6a1c3b78032a1f46ba77a11f58662aee7
Jagdeep Singh <jdecode@gmail.com>
2020-10-07 11:09:46 +0530

    Add "Login with GitHub" link

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -7,4 +7,7 @@
     </fieldset>
     <?= $this->Form->button(__('Login')); ?>
     <?= $this->Form->end() ?>
+    <div class="text-center">
+        <a href="/github/oauth">Login with GitHub</a>
+    </div>
 </div>
