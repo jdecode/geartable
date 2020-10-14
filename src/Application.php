b0fc17d880b9cb9d9c55d8f3a3f2a5eb38af3c88
Jagdeep Singh <jdecode@gmail.com>
2020-10-14 23:53:45 +0530

    Change login/logout URLs for Authentication plugin

--- src/Application.php
+++ src/Application.php
@@ -53,7 +53,7 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
 
         // Define where users should be redirected to when they are not authenticated
         $service->setConfig([
-                                'unauthenticatedRedirect' => '/users/login',
+                                'unauthenticatedRedirect' => '/login',
                                 'queryParam' => 'redirect',
                             ]);
 
@@ -65,7 +65,7 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
         $service->loadAuthenticator('Authentication.Session');
         $service->loadAuthenticator('Authentication.Form', [
             'fields' => $fields,
-            'loginUrl' => '/users/login'
+            'loginUrl' => '/login'
         ]);
 
         // Load identifiers
